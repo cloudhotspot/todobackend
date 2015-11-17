3e66d60869716a0de697dfae253d2e4bdee8a43e
Justin Menga <justin.menga@gmail.com>
2015-11-18 02:47:29 +1300

    Refactor to clean up if job succeeds or fails

--- flow.groovy
+++ flow.groovy
@@ -4,22 +4,27 @@ def src = 'https://github.com/cloudhotspot/todobackend.git'
 node {
     git url: src
 
-    stage 'Run unit/integration tests'
     try {
-        sh 'make test'
-    } catch(all) {
+        stage 'Run unit/integration tests'
+        try { sh 'make test' } 
+        catch(all) {
+            step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
+            error 'Test Failure'
+        }
+        
+        stage 'Build application artefacts'
+        sh 'make build'
+
+        stage 'Create release environment and run acceptance tests'
+        try { sh 'make release' }
+        catch(all) {
+            step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
+            error 'Test Failure'
+        }
         step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
-        error 'Test Failure'
     }
-    
-    stage 'Build application artefacts'
-    sh 'make build'
-
-    stage 'Create release environment and run acceptance tests'
-    sh 'make release'
-
-    stage 'Clean up'
-    sh 'make clean'
-
-    step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
+    finally {
+        stage 'Clean up'
+        sh 'make clean'
+    }
 }
